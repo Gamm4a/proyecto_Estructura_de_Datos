c58024d86a77a8e66455986578b2f1b0f782e205
Camila-Zubia <cami.zubia@gmail.com>
2025-05-16 20:30:41 -0700

    Update FInscripciones.java

--- Negocio/src/main/java/Facade/FInscripciones.java
+++ Negocio/src/main/java/Facade/FInscripciones.java
@@ -5,12 +5,49 @@
 package Facade;
 
 import Interfaz.IInscripciones;
-
+import ObjetosNegocio.Cursos;
+import ObjetosNegocio.Estudiantes;
+import Implementaciones.ArbolBinarioBusqueda;
+import Implementaciones.Diccionario;
 
 /**
  *
  * @author Luis Rafael
  */
 public class FInscripciones implements IInscripciones{
+
+    private Diccionario<String, Cursos> cursos;
+    private ArbolBinarioBusqueda<Estudiantes> estudiantes;
+
+    @Override
+    public void inscribirEstudianteEnCurso(String matricula, String clave) {
+        Cursos cursoInscribir = cursos.get(clave);
+        Estudiantes estudiante = estudiantes.buscarPorAtributo(e -> e.getMatricula(), matricula);
+        cursoInscribir.getInscritos().agregarUltimo(estudiante);
+        cursoInscribir.getRolEstudiantes().agregar(estudiante);
+    }
+
+    @Override
+    public void mostrarInscritos(String clave) {
+        Cursos cursoMostrar = cursos.get(clave);
+        cursoMostrar.getInscritos().imprimir();
+    }
     
+    @Override
+    public void mostrarListaEspera(String clave, int cantidad) {
+        Cursos cursoMostrarEspera = cursos.get(clave);
+        cursoMostrarEspera.getListaEspera().imprimirCant(cantidad);
+    }
+
+    @Override
+    public void recorrerListaEsperaAdelante(String clave) {
+        Cursos cursoMostrarEspera = cursos.get(clave);
+        cursoMostrarEspera.getListaEspera().imprimir();
+    }
+
+    @Override
+    public void recorrerListaEsperaAtras(String clave) {
+        Cursos cursoMostrarEspera = cursos.get(clave);
+        cursoMostrarEspera.getListaEspera().imprimirReversa();
+    }
 }
