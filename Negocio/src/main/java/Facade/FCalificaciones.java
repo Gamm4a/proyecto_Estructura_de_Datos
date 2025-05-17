d493c6d286d095e51e0d4f1142f1fb42bd34ef2c
Camila-Zubia <cami.zubia@gmail.com>
2025-05-16 20:46:21 -0700

    Update FCalificaciones.java

--- Negocio/src/main/java/Facade/FCalificaciones.java
+++ Negocio/src/main/java/Facade/FCalificaciones.java
@@ -4,8 +4,10 @@
  */
 package Facade;
 
+import Implementaciones.ArbolBinarioBusqueda;
+import Implementaciones.ArregloCalificaciones;
 import Interfaz.ICalificaciones;
-
+import ObjetosNegocio.Estudiantes;
 
 
 /**
@@ -13,5 +15,40 @@ import Interfaz.ICalificaciones;
  * @author Luis Rafael
  */
 public class FCalificaciones implements ICalificaciones{
-    
+
+    private ArbolBinarioBusqueda<Estudiantes> arbol = new ArbolBinarioBusqueda<>();
+
+    @Override
+    public void agregarCalificacion(String matricula, double calificacion) {
+        Estudiantes estudiante = arbol.buscarPorAtributo(e -> e.getMatricula(), matricula);
+        estudiante.getCalificaciones().agregarElemento(calificacion);
+    }
+
+    @Override
+    public double calcularPromedio(String matricula) {
+        Estudiantes estudiante = arbol.buscarPorAtributo(e -> e.getMatricula(), matricula);
+        ArregloCalificaciones<Double> calificaciones = estudiante.getCalificaciones();
+        double suma = 0.0;
+        if (calificaciones.getTam() == 0) {
+            return 0.0;
+        }
+        for (int i = 0; i < calificaciones.getTam(); i++) {
+            suma += calificaciones.obtenerElemento(i);
+        }
+        return suma / calificaciones.getTam();
+    }
+
+    @Override
+    public boolean modificarCalificacion(String matricula, double calificacion, double calNueva) {
+        Estudiantes estudiante = arbol.buscarPorAtributo(e -> e.getMatricula(), matricula);
+        ArregloCalificaciones<Double> calificaciones = estudiante.getCalificaciones();
+        return calificaciones.modificarElemento(calificacion, calNueva);
+    }
+
+    @Override
+    public void eliminarCalificacion(String matricula, int posicion) {
+        Estudiantes estudiante = arbol.buscarPorAtributo(e -> e.getMatricula(), matricula);
+        ArregloCalificaciones<Double> calificaciones = estudiante.getCalificaciones();
+        calificaciones.eliminarElemento(posicion);
+    }
 }
