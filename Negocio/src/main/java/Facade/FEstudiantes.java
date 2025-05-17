57cdb2c67c9ddfed177ad8fa9dbdba165c4b51d0
Camila-Zubia <cami.zubia@gmail.com>
2025-05-16 20:29:44 -0700

    Update FEstudiantes.java

--- Negocio/src/main/java/Facade/FEstudiantes.java
+++ Negocio/src/main/java/Facade/FEstudiantes.java
@@ -3,43 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Facade;
-import Implementaciones.ArbolBinarioBusqueda;
-import Implementaciones.ArregloCalificaciones;
-import Interfaz.IEstudiantes;
 
+import Implementaciones.ArbolBinarioBusqueda;
+import Interfaz.IEstudiantes;
 import ObjetosNegocio.Estudiantes;
+
 /**
  *
  * @author Luis Rafael
  */
 public class FEstudiantes implements IEstudiantes{
-    private ArbolBinarioBusqueda arbol;
-    private ArregloCalificaciones<Double> calificaciones;
+    private ArbolBinarioBusqueda<Estudiantes> arbol = new ArbolBinarioBusqueda<>();
     
     @Override
-    public void agregarCalificacion(double calificacion) {
-        calificaciones.agregarElemento(calificacion);
+    public void registrarEstudiante(Estudiantes estudiante) {
+        arbol.insertar(estudiante);
     }
     
     @Override
-    public double calcularPromedio() {
-        double suma = 0.0;
-        if (calificaciones.getTam() == 0) {
-            return 0.0;
-        }
-        for (int i = 0; i < calificaciones.getTam(); i++) {
-            suma += calificaciones.obtenerElemento(i);
-        }
-        return suma / calificaciones.getTam();
-    }
-    
-    @Override
-    public boolean modificarCalificacion(double calificacion, double calNueva) {
-        return calificaciones.modificarElemento(calificacion, calNueva);
-    }
-    
-    @Override
-    public void registrarEstudiante(Estudiantes estudiante){
-        arbol.insertar(estudiante.getMatricula());
+    public Estudiantes buscarEstudiante(String matricula){
+        return arbol.buscarPorAtributo(e -> e.getMatricula(), matricula);
     }
 }
