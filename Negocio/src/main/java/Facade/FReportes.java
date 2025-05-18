f86a4e48d77aa4c5cf97b29bceb93f3736102a21
Camila-Zubia <cami.zubia@gmail.com>
2025-05-17 21:24:23 -0700

    Update FReportes.java

--- Negocio/src/main/java/Facade/FReportes.java
+++ Negocio/src/main/java/Facade/FReportes.java
@@ -4,13 +4,57 @@
  */
 package Facade;
 
+import Implementaciones.ArbolAVL;
+import Implementaciones.ArbolBinarioBusqueda;
+import Implementaciones.ArbolBinarioBusqueda.Nodo;
+import Implementaciones.Diccionario;
 import Interfaz.IReportes;
-
+import ObjetosNegocio.Calificaciones;
+import ObjetosNegocio.Cursos;
+import ObjetosNegocio.Estudiantes;
 
 /**
  *
  * @author Luis Rafael
  */
 public class FReportes implements IReportes{
+
+    private Diccionario<String, Cursos> cursos;
+    private ArbolAVL<Calificaciones> promediosEstudiantes;
+    ArbolBinarioBusqueda<Estudiantes> arbol;
     
+    @Override
+    public Estudiantes rotarRol(String clave) {
+        Cursos cursoMoverRol = cursos.get(clave);
+        if (cursoMoverRol.getRolEstudiantes().vacia()) {
+            System.out.println("no hay estudiantes para asignar rol");
+        }
+        cursoMoverRol.setLider(cursoMoverRol.getLider().getSig());
+        return cursoMoverRol.getLider().getDato();
+    }
+
+    @Override
+    public void listarPorPromedio() {
+        promediosEstudiantes = new ArbolAVL<>();
+
+        llenarAVLConPromedios(arbol.raiz);
+
+        System.out.println("Estudiantes ordenados por promedio:");
+        promediosEstudiantes.inOrder();
+    }
+
+    private void llenarAVLConPromedios(Nodo<Estudiantes> nodo) {
+        if (nodo == null) {
+            return;
+        }
+
+        llenarAVLConPromedios(nodo.getIzq());
+
+        Estudiantes estudiante = nodo.getDato();
+
+        Calificaciones cal = new Calificaciones(estudiante);
+        promediosEstudiantes.insertar(cal);
+
+        llenarAVLConPromedios(nodo.getDer());
+    }
 }
