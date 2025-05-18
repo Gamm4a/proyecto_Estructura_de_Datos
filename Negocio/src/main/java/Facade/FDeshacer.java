19a4f710a90c14d64f2b551e54ade30ebb81681e
Camila-Zubia <cami.zubia@gmail.com>
2025-05-18 12:30:24 -0700

    Update FDeshacer.java

--- Negocio/src/main/java/Facade/FDeshacer.java
+++ Negocio/src/main/java/Facade/FDeshacer.java
@@ -4,10 +4,13 @@
  */
 package Facade;
 
+import Implementaciones.ArbolBinarioBusqueda;
+import Implementaciones.Diccionario;
 import Implementaciones.Pila;
 import Interfaz.IDeshacer;
 import ObjetosNegocio.Accion;
-
+import ObjetosNegocio.Cursos;
+import ObjetosNegocio.Estudiantes;
 
 
 /**
@@ -16,7 +19,19 @@ import ObjetosNegocio.Accion;
  */
 public class FDeshacer implements IDeshacer{
     
+    private Diccionario<String, Cursos> cursos;
+    private ArbolBinarioBusqueda<Estudiantes> estudiantes;
     private static Pila<Accion> pila = new Pila<>();
+
+    /**
+     * contructor que crea una instancia de la clase con estructuras compartidas
+     * @param cursos
+     * @param estudiantes 
+     */
+    public FDeshacer(Diccionario<String, Cursos> cursos, ArbolBinarioBusqueda<Estudiantes> estudiantes) {
+        this.cursos = cursos;
+        this.estudiantes = estudiantes;
+    }
     
     /**
      * metodo para registra una accion
@@ -31,7 +46,70 @@ public class FDeshacer implements IDeshacer{
      * @return 
      */
     @Override
-    public String deshacerAccion() {
-        return null;
+    public void deshacerUltimaAccion() {
+        if (pila.vacia()) {
+            System.out.println("No hay acciones para deshacer.");
+            return;
+        }
+        Accion accion = pila.pop();
+        Estudiantes estudiante = accion.getEstudiante();
+        Cursos curso = accion.getCurso();
+        
+        switch (accion.getTipo()) {
+
+            case REGISTRO_ESTUDIANTE -> {
+                estudiantes.eliminar(estudiante);
+                System.out.println("Se deshizo el registro del estudiante: " + estudiante.getNombre());
+            }
+
+            case ELIMINAR_ESTUDIANTE -> {
+                estudiantes.insertar(estudiante);
+                System.out.println("Se restauró el estudiante eliminado: " + estudiante.getNombre());
+            }
+
+            case INSCRIPCION -> {
+                if (curso != null) {
+                    curso.getInscritos().eliminar(estudiante);
+                    curso.getRolEstudiantes().eliminar(estudiante);
+                    System.out.println("Se deshizo la inscripción del estudiante en el curso.");
+                }
+            }
+
+            case BAJA_ESTUDIANTE -> {
+                if (curso != null) {
+                    curso.getInscritos().agregarUltimo(estudiante);
+                    System.out.println("Se revirtió la baja del estudiante en el curso.");
+                }
+            }
+
+            case CALIFICACION_AGREGADA -> {
+                if (estudiante != null && accion.getPosicionCalificacion() >= 0) {
+                    estudiante.getCalificaciones().eliminarElemento(accion.getPosicionCalificacion());
+                    System.out.println("Se deshizo la calificación agregada.");
+                }
+            }
+
+            case CALIFICACION_MODIFICADA -> {
+                if (estudiante != null && accion.getPosicionCalificacion() >= 0) {
+                    estudiante.getCalificaciones().modificarElemento(
+                            accion.getCalificacionNueva(),
+                            accion.getCalificacionAnterior()
+                    );
+                    System.out.println("Se deshizo la modificación de calificación.");
+                }
+            }
+
+            case AGREGAR_CURSO -> {
+                cursos.remove(accion.getCurso().getClave());
+                System.out.println("Se deshizo la creación del curso: " + accion.getCurso().getNombre());
+            }
+
+            case ELIMINAR_CURSO -> {
+                cursos.put(accion.getCurso().getClave(), accion.getCurso());
+                System.out.println("Se restauró el curso eliminado: " + accion.getCurso().getNombre());
+            }
+
+            default -> System.out.println("Acción no reconocida.");
+        }
     }
 }
