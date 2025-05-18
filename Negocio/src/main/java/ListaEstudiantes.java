85fd62b8d58d5ef9bad917dd75586d6a390c6e88
rapayel <luisrafaellagarda@gmail.com>
2025-05-17 18:26:13 -0600

    Add files via upload

---
+++ Negocio/src/main/java/ListaEstudiantes.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Implementaciones;
+
+/**
+ *
+ * @author Camila Zubía
+ * @param <T> tipo generico de elemento almacenado en una lista
+ */
+public class ListaEstudiantes<T> {
+
+    private Nodo<T> p;
+    private int tamaño;
+
+    public static class Nodo<T> {
+
+        private T dato;
+        Nodo<T> sig;
+
+        public Nodo(T dato) {
+            this.dato = dato;
+            this.sig = null;
+        }
+
+        public T getDato() {
+            return dato;
+        }
+
+        public void setDato(T dato) {
+            this.dato = dato;
+        }
+
+        public Nodo<T> getSig() {
+            return sig;
+        }
+
+        public void setSig(Nodo<T> sig) {
+            this.sig = sig;
+        }
+    }
+    
+    /**
+     * crea el inicio de la lista
+     * @param dato valor del nodo
+     */
+    public ListaEstudiantes() {
+        this.p = null;
+        this.tamaño = 0;
+    }
+    
+    /**
+     * agregar un nodo al final de la lista 
+     * @param dato valor del nodo
+     */
+    public void agregarUltimo(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        Nodo<T> actual = p;
+        while (actual.sig != null) {
+            actual = actual.sig;
+        }
+        actual.sig = nuevo;
+        tamaño++;
+    }
+
+    /**
+    * eliminar un nodo de la lista 
+    */
+    public void eliminar(T dato) {
+        if (p == null) {
+            return;
+        }else if (p.getDato().equals(dato)) {
+            p = p.getSig();
+            tamaño--;
+            return;
+        }
+
+        Nodo<T> actual = p;
+        Nodo<T> anterior = null;
+
+        while (actual != null && !actual.getDato().equals(dato)) {
+            anterior = actual;
+            actual = actual.getSig();
+        }
+
+        if (actual != null) {
+            anterior.setSig(actual.getSig());
+            tamaño--;
+        }
+    }
+    
+    /**
+     * imprime los nodos de la lista
+     */
+    public void imprimir() {
+        Nodo<T> actual = p;
+        while (actual != null) {
+            System.out.print(actual.getDato() + " -> ");
+            actual = actual.sig;
+        }
+        System.out.println("null");
+    }
+    
+    /**
+     * regrese el tamaño de la lista
+     * @return tamaño de la lista
+     */
+    public int getTamaño() {
+        return tamaño;
+    }
+}
