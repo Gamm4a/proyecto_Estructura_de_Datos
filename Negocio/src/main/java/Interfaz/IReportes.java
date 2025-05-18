970f837b9f4c6de1915d0930696e63707b88183c
Camila-Zubia <cami.zubia@gmail.com>
2025-05-17 23:47:11 -0700

    Update IReportes.java

--- Negocio/src/main/java/Interfaz/IReportes.java
+++ Negocio/src/main/java/Interfaz/IReportes.java
@@ -2,18 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Interfaz;
-
-import ObjetosNegocio.Estudiantes;
+package Interfaces;
 
 /**
  *
- * @author 52644
+ * @author Luis Rafael
  */
 public interface IReportes {
     
     void listarPorPromedio();
     
-    Estudiantes rotarRol(String clave);
+    void rotarRol(String clave);
 }
-
